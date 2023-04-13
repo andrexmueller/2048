0ab27543e6703784b74461d55bc9dd7e6f2cc72b
andrexmueller <30375813+andrexmueller@users.noreply.github.com>
2023-04-13 09:53:16 -0300

    Update main.js
    swipe detection on android browser

--- main.js
+++ main.js
@@ -260,3 +260,23 @@ window.addEventListener('keydown', (e) => {
     const eventos = ['ArrowUp', 'ArrowDown', 'ArrowLeft', 'ArrowRight']
     if (eventos.includes(e.key)) matriz.move(e.key)
 } )
+
+
+// teste swipe on android browser
+
+let touchstartX = 0
+let touchendX = 0
+    
+function checkDirection() {
+  if (touchendX < touchstartX) alert('swiped left!')
+  if (touchendX > touchstartX) alert('swiped right!')
+}
+
+document.addEventListener('touchstart', e => {
+  touchstartX = e.changedTouches[0].screenX
+})
+
+document.addEventListener('touchend', e => {
+  touchendX = e.changedTouches[0].screenX
+  checkDirection()
+})
